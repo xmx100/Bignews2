a464fac94764c444ef51387de620a891415b1eee
xmx <994212812@qq.com>
2020-09-17 18:59:34 +0800

    完成个人中心功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,5 @@
 $(function () {
-  layer = layui.layer;
-  // 获取用户的基本信息
-  getUserInfo();
-  //   获取用户的基本信息
-  function getUserInfo() {
-    $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
-      // headers 就是请求头配置对象
-
-      success: function (res) {
-        if (res.status !== 0) {
-          return layer.msg('获取用户信息失败！');
-        }
-        // 调用 renderAvatar 渲染用户的头像
-        renderAvatar(res.data);
-      },
-    });
-  }
-  // 渲染用户的头像
-  function renderAvatar(user) {
-    // 1. 获取用户的名称
-    var name = user.nickname || user.username;
-    // 2. 设置欢迎的文本
-    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
-    if (user.user_pic != null) {
-      // 3.1 渲染图片头像
-      $('.layui-nav-img').attr('src', user.user_pic).show();
-      $('.text-avatar').hide();
-    } else {
-      // 3.2 渲染文本头像
-      var first = name[0].toUpperCase();
-      $('.text-avatar').html(first).show();
-      $('.layui-nav-img').hide();
-    }
-  }
+  var layer = layui.layer;
   //   实现退出功能
   $('#btnLogout').on('click', function () {
     layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
@@ -47,3 +12,39 @@ $(function () {
     });
   });
 });
+
+// 获取用户的基本信息
+getUserInfo();
+//   获取用户的基本信息
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers 就是请求头配置对象
+
+    success: function (res) {
+      if (res.status !== 0) {
+        return layer.msg('获取用户信息失败！');
+      }
+      // 调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data);
+    },
+  });
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+  // 1. 获取用户的名称
+  var name = user.nickname || user.username;
+  // 2. 设置欢迎的文本
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+  if (user.user_pic != null) {
+    // 3.1 渲染图片头像
+    $('.layui-nav-img').attr('src', user.user_pic).show();
+    $('.text-avatar').hide();
+  } else {
+    // 3.2 渲染文本头像
+    var first = name[0].toUpperCase();
+    $('.text-avatar').html(first).show();
+    $('.layui-nav-img').hide();
+  }
+}
